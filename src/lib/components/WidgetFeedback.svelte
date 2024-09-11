04411d8ea2d088e3f1db164f86e9260b5a133db5
arh007 <arifrh007007007@gmail.com>
2024-09-11 21:38:29 +0700

    baru

--- src/lib/components/WidgetFeedback.svelte
+++ src/lib/components/WidgetFeedback.svelte
@@ -1,4 +1,4 @@
 <div {...$$props}>
     <h4>Umpan Balik</h4>
-    <span>Kirimkan saja melalui <a href="mailto: arahmanda21@gmail.com" style="text-decoration: underline;">Email</a> ini</span>
+    <span>Kirimkan saja melalui <a href="mailto: arifrh007007007@gmail.com" style="text-decoration: underline;">Email</a> ini</span>
 </div>
